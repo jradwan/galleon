1908f878ef51fc34be4c63c888b1f4b5a4ab2b5a
javahmo <>
2006-09-27 05:33:37 +0000

    Updates

--- galleon/src/org/lnicholls/galleon/skins/Descriptor.java
+++ galleon/src/org/lnicholls/galleon/skins/Descriptor.java
@@ -1,40 +1,46 @@
 package org.lnicholls.galleon.skins;
-
 /*
  * Copyright (C) 2005 Leon Nicholls
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * 
+ *
  * See the file "COPYING" for more details.
  */
 
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class Descriptor {
-    
+
     public Descriptor()
     {
         mImages = new ArrayList();
+        mColors = new ArrayList();
     }
-    
+
     public void getImages(java.util.List value) {
         mImages = value;
+    }
+    public void getColors(java.util.List value) {
+        mColors = value;
     }
 
     public java.util.List getImages() {
         return mImages;
     }
-    
+    public java.util.List getColors() {
+        return mColors;
+    }
+
     public Image getImage(String id)
     {
         for (int i = 0; i < mImages.size(); i++) {
@@ -45,11 +51,24 @@ public class Descriptor {
         }
         return null;
     }
+    public Color getColor(String id)
+    {
+        for (int i = 0; i < mColors.size(); i++) {
+            Color color = (Color) mColors.get(i);
+            if (color.getId().toLowerCase().equals(id.toLowerCase())) {
+                return color;
+            }
+        }
+        return null;
+    }
 
     public void addImage(Image value) {
         mImages.add(value);
     }
-    
+    public void addColor(Color value) {
+        mColors.add(value);
+    }
+
     public static class Image {
         public Image() {
 
@@ -78,7 +97,29 @@ public class Descriptor {
         private String mSource;
 
         private String mId;
-    }    
-    
+    }
+    public static class Color {
+        public Color() {
+        }
+        public String getValue() {
+            return mValue;
+        }
+        public void setValue(String value) {
+            mValue = value;
+        }
+        public String getId() {
+            return mId;
+        }
+        public void setId(String value) {
+            mId = value;
+        }
+        public String toString() {
+            return ToStringBuilder.reflectionToString(this);
+        }
+        private String mValue;
+        private String mId;
+    }
+
     private List mImages;
+    private List mColors;
 }

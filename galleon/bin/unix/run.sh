b635681382bf7182715e3e99e07c047556b5b833
s2kdave <>
2007-09-02 19:34:04 +0000

    added hdphotos stub jar.  Added unix install makefile and service layer wrapper

--- galleon/bin/unix/run.sh
+++ galleon/bin/unix/run.sh
@@ -2,20 +2,4 @@
 #
 # Run the Galleon server
 #
-# You may have to set JAVA_HOME to the correct value for your system
-#JAVA_HOME=/usr/java/jre
-mydir=`dirname "$0"`
-cd "$mydir"
-CLASSPATH=../conf
-for j in ../lib/*.jar 
-do 
-CLASSPATH=$CLASSPATH:$j 
-done 
-
-OPTION=""
-if [ -f "$JAVA_HOME/bin/server" ]; then
-OPTION=-server
-fi
-mkdir -p ../data/tmp
-java $OPTION -cp $CLASSPATH -Xms64m -Xmx64m -Djava.net.preferIPv4Stack=true -Djava.io.tmpdir="`pwd`/../data/tmp" org.lnicholls.galleon.server.Server
-
+/usr/share/galleon/bin/galleon wrapper.console.loglevel=INFO console

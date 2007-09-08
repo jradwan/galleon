fcfe3b75b4025fac24c8b8fef62de9a535d24a6e
s2kdave <>
2007-09-08 17:40:37 +0000

    Removed event class because as it turns out there already was that functionality in the api.

--- bananas/src/com/tivo/hme/bananas/BApplication.java
+++ bananas/src/com/tivo/hme/bananas/BApplication.java
@@ -8,14 +8,11 @@
 
 package com.tivo.hme.bananas;
 
-import com.tivo.hme.bananas.ext.HmeEventListener;
 import com.tivo.hme.interfaces.IContext;
-import com.tivo.hme.interfaces.ILogger;
 import com.tivo.hme.sdk.Application;
 import com.tivo.hme.sdk.HmeEvent;
 import com.tivo.hme.sdk.View;
 import java.util.Vector;
-import javax.swing.event.EventListenerList;
 
 /**
  * A bananas application.
@@ -62,8 +59,6 @@ abstract public class BApplication extends Application implements IBananas
 
     /** If true, a push/pop occurred recently. */
     private boolean screenChanged;
-    
-    private EventListenerList listeners;
 
     /**
      * Init the application. Subclasses must call super.init().
@@ -172,34 +167,6 @@ abstract public class BApplication extends Application implements IBananas
     {
         this.skin = skin;
     }
-    
-    public void addHmeEventListener(HmeEventListener listener) {
-        if (listeners == null) {
-            listeners = new EventListenerList();
-        }
-        listeners.add(HmeEventListener.class, listener);
-    }
-    
-    public void removeHmeEventListener(HmeEventListener listener) {
-        if (listeners != null) {
-            listeners.remove(HmeEventListener.class, listener);
-        }
-    }
-    
-    protected void fireEventReceived(HmeEvent event) {
-        if (listeners != null) {
-            HmeEventListener[] list = listeners.getListeners(HmeEventListener.class);
-            for (HmeEventListener listener : list) {
-                try {
-                    listener.eventReceived(this, event);
-                } catch (Throwable t) {
-                    log(ILogger.LOG_WARNING, 
-                            "An exception occurred during listener eventReceived: " + 
-                            t.getMessage());
-                }
-            }
-        }
-    }
 
     //
     // event handlers
@@ -210,8 +177,6 @@ abstract public class BApplication extends Application implements IBananas
      */
     protected void dispatchEvent(HmeEvent event)
     {
-        //fire to the event listeners
-        fireEventReceived(event);
         
         switch (event.getOpCode()) {
           case EVT_KEY:
--- bananas/src/com/tivo/hme/bananas/ext/HmeEventListener.java
+++
@@ -1,9 +0,0 @@
-package com.tivo.hme.bananas.ext;
-
-import com.tivo.hme.sdk.HmeEvent;
-import com.tivo.hme.sdk.HmeObject;
-import java.util.EventListener;
-
-public interface HmeEventListener extends EventListener {
-    public void eventReceived(HmeObject object, HmeEvent event);
-}

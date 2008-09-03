17fee6dabe0a37e4f907d43a2aa687322a876b13
johnkohl <>
2008-09-03 04:02:20 +0000

    fixes for OSX builds

--- galleon/distro/osx/build-osx.sh
+++ galleon/distro/osx/build-osx.sh
@@ -3,16 +3,16 @@
 cd `dirname $0`/../..
 sudo rm -rf osx-dir
 mkdir osx-dir
-mkdir -p osx-dir/ROOT/Galleon
-(cd distro/osx/bundles && tar -cf - --exclude=\*~ --exclude=CVS *.app)|(cd osx-dir/ROOT/Galleon && tar xpBf - )
+mkdir -p osx-dir/Galleon/Galleon
+(cd distro/osx/bundles && tar -cf - --exclude=\*~ --exclude=CVS *.app)|(cd osx-dir/Galleon/Galleon && tar xpBf - )
 if [ $# -gt 0 ]; then
     rm -rf build.osx
     ant -Dplatform=osx package
     mv build build.osx
 fi
-    (cd build.osx && tar cf - *) | (cd  osx-dir/ROOT/Galleon/Galleon*.app/Contents/Resources && mkdir Java && cd Java && tar xpBf -) 
-(cd osx-dir/ROOT/Galleon/Conf*.app/Contents/Resources && ln -s ../../../Galleon*.app/Contents/Resources/Java ./Java)
-for i in osx-dir/ROOT/Galleon/*.app; do
+    (cd build.osx && tar cf - *) | (cd  osx-dir/Galleon/Galleon/Galleon*.app/Contents/Resources && mkdir Java && cd Java && tar xpBf -) 
+(cd osx-dir/Galleon/Galleon/Conf*.app/Contents/Resources && ln -s ../../../Galleon*.app/Contents/Resources/Java ./Java)
+for i in osx-dir/Galleon/Galleon/*.app; do
     mkdir "$i"/Contents/MacOS
 # You might be tempted to symlink this file, but that seems to make finder think both galleon & configure galleon are the same app.
 ##echo     cp /System/Library/Frameworks/JavaVM.framework/Versions/Current/Resources/MacOS/JavaApplicationStub "$i"/Contents/MacOS/galleon_java_stub >"$i"/Contents/MacOS/ORIGIN.txt
@@ -21,7 +21,7 @@ for i in osx-dir/ROOT/Galleon/*.app; do
     mkdir "$i"/Contents/Resources/Java/data/tmp
     /Developer/Tools/SetFile -a B "$i"
 done
-mv "osx-dir/ROOT/Galleon/Galleon Server.app"/Contents/Resources/Java/conf/configure.xml "osx-dir/ROOT/Galleon/Galleon Server.app"/Contents/Resources/Java/conf/configure.xml-default
+mv "osx-dir/Galleon/Galleon/Galleon Server.app"/Contents/Resources/Java/conf/configure.xml "osx-dir/Galleon/Galleon/Galleon Server.app"/Contents/Resources/Java/conf/configure.xml-default
 #mkdir -p osx-dir/Install_resources/English.lproj
 (cd distro/osx && tar --exclude=CVS --exclude=\*~ -cf - MainResources)|(cd osx-dir && tar xpBf -)
 #cp distro/osx/MainResources/postflight osx-dir/Install_resources
@@ -36,11 +36,15 @@ sudo chown -R root:admin osx-dir
 #/Developer/Tools/packagemaker -build -proj distro/osx/Galleon.pmproj -p distro/osx/Galleon.pkg -v
 VER="2.5.3"
 
+rm -rf Galleon.pkg
 # Backward compat builds on 10.5, may need to update to native commands?
-/Developer/usr/bin/packagemaker -build \
--f osx-dir/ROOT -ds -v -r osx-dir/MainResources \
--i distro/osx/Info.plist \
--d distro/osx/Description.plist -p Galleon.pkg -v 
+/Developer/usr/bin/packagemaker \
+--root osx-dir/Galleon --target 10.4 --discard-forks --verbose \
+--resources osx-dir/MainResources \
+--info distro/osx/Info.plist \
+--filter .DS_Store \
+--out Galleon.pkg
+# ??? -d distro/osx/Description.plist
 
 VOL="Galleon"
 FILES="Galleon.pkg"
